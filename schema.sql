3ae8e078e13d27ed5478c5926d351d44e5cf9d85
abedb961 <abedbaydoun961@hotmail.com>
2025-04-27 21:29:51 +0300

    added foreign keys and other attributes

--- schema.sql
+++ schema.sql
@@ -3,20 +3,30 @@ USE quiz_app;
 
 CREATE TABLE users (
     id INT AUTO_INCREMENT PRIMARY KEY,
-
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    _admin BOOLEAN DEFAULT 0
 );
 
 CREATE TABLE quizzes (
     id INT AUTO_INCREMENT PRIMARY KEY,
-
+    title VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE questions (
     id INT AUTO_INCREMENT PRIMARY KEY,
-
+    quiz_id INT,
+    question_text TEXT NOT NULL,
+    possible_answers TEXT NOT NULL,
+    correct_answer VARCHAR(255) NOT NULL,
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id)
 );
 
 CREATE TABLE scores (
     id INT AUTO_INCREMENT PRIMARY KEY,
-
+    user_id INT NOT NULL,
+    quiz_id INT NOT NULL,
+    score INT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (quiz_id) REFERENCES quizzes(id)
 );
