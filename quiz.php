f0fe9dab305efac9997f12d9714e5ea8c43f19d7
abedb961 <abedbaydoun961@hotmail.com>
2025-04-27 23:07:41 +0300

    added a create quiz functionality

---
+++ quiz.php
@@ -0,0 +1,18 @@
+<?php
+include ('db.php');
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['create_quiz'])) {
+    $title = $_POST['title'];
+    $description = $_POST['description'];
+
+    $stmt = $conn->prepare("INSERT INTO quizzes (title, description) VALUES (?, ?)");
+    $stmt->bind_param("ss", $title, $description);
+    $stmt->execute();
+
+    if ($stmt->affected_rows > 0) {
+        echo json_encode(['message' => 'Quiz Created!']);
+    } else {
+        echo json_encode(['error' => 'Error! Failed To Create Quiz.']);
+    }
+}
+?>
